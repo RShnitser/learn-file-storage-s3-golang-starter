73db58d0bfc46fb44322bcdd80fe44ee03a99cb2
RShnitser <romanshnitser@hotmail.com>
2025-01-07 11:36:27 -0500

    fix cache middleware

--- cache.go
+++ cache.go
@@ -5,7 +5,6 @@ import "net/http"
 func noCacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-store")
-		w.Header().Set("Cache-Control", "max-age=3600")
 		next.ServeHTTP(w, r)
 	})
 }
